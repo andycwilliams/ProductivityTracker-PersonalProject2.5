6e0af3607cfcc76e94a96966fd69fa0d9e983358
Andy <awilliamscomposing@gmail.com>
2021-09-14 22:48:11 -0700

    Starting dynamic task list creator

--- index.js
+++ index.js
@@ -4,21 +4,55 @@ const client = new MongoClient('mongodb://localhost:27017');
 const db = client.db('birds');
 const collection = db.collection('documents');
 
-const getBirds = (collection) => collection.find({}).toArray();
+// const getBirds = (collection) => collection.find({}).toArray();
 
-const addBird = async (collection) => {
-  const result = await collection.insertMany([
-    { name: 1 },
-    { name: 2 },
-    { name: 3 },
-  ]);
-  console.log(result);
-  return collection;
-};
+// const addBird = async (collection) => {
+//   const result = await collection.insertMany([
+//     { name: 1 },
+//     { name: 2 },
+//     { name: 3 },
+//   ]);
+//   console.log(result);
+//   return collection;
+// };
 
-app.get('/', async function (req, res) {
-  const birds = await getBirds(collection);
-  res.json(birds);
-});
+// app.get('/', async function (req, res) {
+//   const birds = await getBirds(collection);
+//   res.json(birds);
+// });
 
-client.connect().then(() => app.listen(3000));
+// client.connect().then(() => app.listen(3000));
+
+
+
+function taskContainer(){
+  for (let i = 0; i < 10; i++){
+      const itemEl = document.createElement("div");
+      // const hourEl = document.createElement("div");
+      const textAreaEl = document.createElement("textarea");
+      const addItemBtn = document.createElement("button");
+
+      // hourEl.textContent = i + ":00";
+      addItemBtn.innerText = "Add";
+
+      itemEl.setAttribute("id", "hour-", + i);
+      itemEl.classList.add("row", "time-block")
+      // hourEl.classList.add("col-sm-12", "hour");
+      textAreaEl.classList.add("col-sm-11", "description");
+      textAreaEl.setAttribute("id", "Dynamically Generated ID")
+      addItemBtn.classList.add("col-sm-1", "btn", "addItemBtn");
+
+      // if (i < currentTimeEl){
+      //     itemEl.classList.add("past");
+      // } else if (i === currentTimeEl){
+      //     itemEl.classList.add("present");
+      // } else {
+      //     itemEl.classList.add("future");
+      // };
+
+      timeBlockContainer.appendChild(itemEl);
+      // itemEl.appendChild(hourEl);
+      itemEl.appendChild(textAreaEl);
+      itemEl.appendChild(addItemBtn);
+    };
+  };
