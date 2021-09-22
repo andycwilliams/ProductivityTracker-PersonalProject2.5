3964b714c98f56917da6b246df72eef2ee018c07
Andy <awilliamscomposing@gmail.com>
2021-09-21 21:52:30 -0700

    Starting timer function

--- index.js
+++ index.js
@@ -30,9 +30,44 @@ const collection = db.collection('documents');
 
 // client.connect().then(() => app.listen(3000));
 
+const sessionTimer = () => {
+  const sec = 30;
+  const timer = setInterval(function(){
+      document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+      sec--;
+      if (sec < 0) {
+          clearInterval(timer);
+      };
+  }, 1000);
+};
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = 0;
+          // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+      }
+  }, 1000);
+}
+
+window.onload = function () {
+  var time = 60 / 2, // your time in seconds here
+      display = document.querySelector('#safeTimerDisplay');
+  startTimer(time, display);
+};
 
 
-function taskContainer(){
+
+const taskContainer = () => {
   for (let i = 0; i < 10; i++){
       const itemEl = document.createElement("div");
       // const hourEl = document.createElement("div");
