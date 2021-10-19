24db575f4f0d565e5bc8e8ba62598134dd5e68c7
Andy <awilliamscomposing@gmail.com>
2021-10-19 16:04:56 -0700

    Deleting duplicate code

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@
 
 const minutesBtn = document.querySelector("#chooseMin");
 const timerBtn = document.querySelector("#timer");
-const timerDisplay = document.querySelector('#timerDisplay');
+const timerDisplay = document.querySelector("#timerDisplay");
 const paustimerDisplayrBtn = document.querySelector("#paustimerDisplayr");
 const resetTimerBtn = document.querySelector("#resetTimer");
 const toggleSoundBtn = document.querySelector("#toggleSound");
@@ -21,31 +21,6 @@ let mode = "light";
 // Don't worry about handlebars. Keep it as simple as possible
 // Point is to get familiar with serverless and implement it on a project
 
-// -------------------------------- Choose Time Function --------------------------------
-
-const chooseMinutes = () => {
-  minutesBtn.onclick = function() {
-    const userNo = document.querySelector("#inputNo").value;
-    console.log(`userNo: ${userNo}`);
-    startTimer(userNo, timerDisplay);
-  };
-};
-minutesBtn.addEventListener("click", chooseMinutes());
-
-// -------------------------------- Pause Timer --------------------------------
-
-const paustimerDisplayr = () => {
-  //
-};
-paustimerDisplayrBtn.addEventListener("click", paustimerDisplayr());
-
-// -------------------------------- Reset Timer --------------------------------
-
-const resetTimer = () => {
-  clearInterval(timerBtn);
-};
-resetTimerBtn.addEventListener("click", resetTimer());
-
 // -------------------------------- Toggle Sound --------------------------------
 
 const toggleSound = () => {
@@ -55,181 +30,91 @@ toggleSoundBtn.addEventListener("click", toggleSound());
 
 // -------------------------------- Light/Dark Mode --------------------------------
 
-nightlight.addEventListener("click", function() {
+nightlight.addEventListener("click", function () {
   if (mode === "dark") {
     mode = "light";
     // container.setAttribute("class", "light");
-    console.log("Morning already?")
-  }
-  else {
+    console.log("Morning already?");
+  } else {
     mode = "dark";
     // container.setAttribute("class", "dark");
-    console.log("Nighty night!")
+    console.log("Nighty night!");
   }
 });
-
-// -------------------------------- Timer Function --------------------------------
-
-function startTimer(duration, display) {
-  // if (timerBtn.paused){    
-  //   timerBtn.play();
-  //   console.log("Timer start!")
-  // } else {
-  //   timerBtn.pause("Timer end!");
-  // }
-
-  let timer = duration, minutes, seconds;
-  setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    console.log("Tick");
-
-    if (--timer < 0) {
-        timer = duration;
-        console.log("Time's up!");
-        // playSound();
-        // clearInterval(intervalEl);
-    };
-  }, 1000);
-};
 
 // -------------------------------- Random Qoute Generator --------------------------------
 
 const randomQuoteArr = [
-  'In the evening, write down the most important things you need to do the next day.',
-  'Don\'t write down so much that you overwhelm yourself.',
-  'Focus on process, not product.',
-  'Recall is one of the most effective learning methods.',
+  "In the evening, write down the most important things you need to do the next day.",
+  "Don't write down so much that you overwhelm yourself.",
+  "Focus on process, not product.",
+  "Recall is one of the most effective learning methods.",
   '"Eat your frogs first." Do the most unpleasant tasks first so they\'re out of the way.',
-  'Time is not an enemy. When handled correctly, it can even be turned into an ally.',
-  'Knowing when to take breaks is just as important as the work itself!',
-  'Flash cards are cheap and effective ways to drill yourself on smaller concepts.'
+  "Time is not an enemy. When handled correctly, it can even be turned into an ally.",
+  "Knowing when to take breaks is just as important as the work itself!",
+  "Flash cards are cheap and effective ways to drill yourself on smaller concepts.",
 ];
 
 const randomQuoteGenerator = () => {
-  const randomNo = Math.floor(Math.random() * (randomQuoteArr.length));
-  document.querySelector('#randomTip').innerHTML = randomQuoteArr[randomNo];
+  const randomNo = Math.floor(Math.random() * randomQuoteArr.length);
+  document.querySelector("#randomTip").innerHTML = randomQuoteArr[randomNo];
 };
 
 // -------------------------------- Task Generator --------------------------------
 
 const taskListGenerator = () => {
-  for (let i = 0; i < 10; i++){
-      const itemEl = document.createElement("div");
-      // const hourEl = document.createElement("div");
-      const textAreaEl = document.createElement("textarea");
-      const addItemminutesBtn = document.createElement("button");
+  for (let i = 0; i < 10; i++) {
+    const itemEl = document.createElement("div");
+    // const hourEl = document.createElement("div");
+    const textAreaEl = document.createElement("textarea");
+    const addItemminutesBtn = document.createElement("button");
 
-      // hourEl.textContent = i + ":00";
-      addItemminutesBtn.innerText = "Add";
+    // hourEl.textContent = i + ":00";
+    addItemminutesBtn.innerText = "Add";
 
-      itemEl.setAttribute("id", "hour-", + i);
-      itemEl.classList.add("row", "time-block")
-      // hourEl.classList.add("col-sm-12", "hour");
-      textAreaEl.classList.add("col-sm-11", "description");
-      textAreaEl.setAttribute("id", "Dynamically Generated ID")
-      addItemminutesBtn.classList.add("col-sm-1", "minutesBtn", "addItemminutesBtn");
+    itemEl.setAttribute("id", "hour-", +i);
+    itemEl.classList.add("row", "time-block");
+    // hourEl.classList.add("col-sm-12", "hour");
+    textAreaEl.classList.add("col-sm-11", "description");
+    textAreaEl.setAttribute("id", "Dynamically Generated ID");
+    addItemminutesBtn.classList.add(
+      "col-sm-1",
+      "minutesBtn",
+      "addItemminutesBtn"
+    );
 
-      // if (i < currentTimeEl){
-      //     itemEl.classList.add("past");
-      // } else if (i === currentTimeEl){
-      //     itemEl.classList.add("present");
-      // } else {
-      //     itemEl.classList.add("future");
-      // };
+    // if (i < currentTimeEl){
+    //     itemEl.classList.add("past");
+    // } else if (i === currentTimeEl){
+    //     itemEl.classList.add("present");
+    // } else {
+    //     itemEl.classList.add("future");
+    // };
 
-      taskContainer.appendChild(itemEl);
-      // itemEl.appendChild(hourEl);
-      itemEl.appendChild(textAreaEl);
-      itemEl.appendChild(addItemminutesBtn);
-  };
+    taskContainer.appendChild(itemEl);
+    // itemEl.appendChild(hourEl);
+    itemEl.appendChild(textAreaEl);
+    itemEl.appendChild(addItemminutesBtn);
+  }
 };
 
 randomQuoteGenerator();
 taskListGenerator();
 
-// -------------------------------- Stopwatch --------------------------------
+// -------------------------------- Add Task --------------------------------
 
-{/* <div id="stopwatch">
-  <!-- CURRENT TIME -->
-  <div id="sw-time">00:00:00</div>
- 
-  <!-- CONTROLS -->
-  <input type="button" value="Reset" id="sw-rst" disabled/>
-  <input type="button" value="Start" id="sw-go" disabled/>
-</div> */}
-
-const startStop = {
-  // (A) INITIALIZE
-  timerShow: null, // HTML time display
-  timerReset: null, // HTML reset button
-  timerBegin: null, // HTML start/stop button
-  init: function () {
-    // (A1) GET HTML ELEMENTS
-    startStop.timerShow = document.getElementById("startStop-show");
-    startStop.timerReset = document.getElementById("startStop-reset");
-    startStop.timerBegin = document.getElementById("startStop-begin");
-
-    // (A2) ENABLE BUTTON CONTROLS
-    startStop.timerReset.addEventListener("click", startStop.reset);
-    startStop.timerReset.disabled = false;
-    startStop.timerBegin.addEventListener("click", startStop.start);
-    startStop.timerBegin.disabled = false;
-  },
-
-  // (B) TIMER ACTION
-  timer: null, // timer object
-  now: 0, // current elapsed time
-  tick: function () {
-    // (B1) CALCULATE HOURS, MINS, SECONDS
-    startStop.now++;
-    const remain = startStop.now;
-    const hours = Math.floor(remain / 3600);
-    remain -= hours * 3600;
-    const mins = Math.floor(remain / 60);
-    remain -= mins * 60;
-    const secs = remain;
-
-    // (B2) UPDATE THE DISPLAY TIMER
-    if (hours < 10) { hours = "0" + hours; };
-    if (mins < 10) { mins = "0" + mins; };
-    if (secs < 10) { secs = "0" + secs; };
-    startStop.timerShow.innerHTML = hours + ":" + mins + ":" + secs;
-  },
-  
-  // (C) START!
-  start: function () {
-    startStop.timer = setInterval(startStop.tick, 1000);
-    startStop.timerBegin.value = "Stop";
-    startStop.timerBegin.removeEventListener("click", startStop.start);
-    startStop.timerBegin.addEventListener("click", startStop.stop);
-  },
-
-  // (D) STOP
-  stop: function () {
-    clearInterval(startStop.timer);
-    startStop.timer = null;
-    startStop.timerBegin.value = "Start";
-    startStop.timerBegin.removeEventListener("click", startStop.stop);
-    startStop.timerBegin.addEventListener("click", startStop.start);
-  },
-
-  // (E) RESET
-  reset: function () {
-    if (startStop.timer != null){ 
-      startStop.stop(); 
-    };
-    startStop.now = -1;
-    startStop.tick();
-  }
-};
-window.addEventListener("load", startStop.init);
+// document.getElementById("tweet").addEventListener("click", () => {
+//   const getTweetData = () => ({
+//       author: document.getElementById("author").value,
+//       body: document.getElementById("body").value,
+//   });
+//   console.log("sending a tweet: ", getTweetData());
+//   axios
+//       .post("/.netlify/functions/tweeting-api", {
+//           body: JSON.stringify(getTweetData()),
+//       })
+//       .then(console.log);
+// });
 
 // ----------------------------------------------------------------
 
