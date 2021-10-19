e770aeb2779d4f0a697f636c596eb373585231d8
Andy <awilliamscomposing@gmail.com>
2021-10-18 17:36:03 -0700

    Working on routes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const Task = require("../models/tasklist.js");
 
+router.get("/", async (req, res) => {
+  console.log(`Method ${req.method} received.`);
+  const allTasks = await Task.findAll().catch((err) => {
+    res.status(500).json("Cannot find tasks.");
+  });
+  res.status(200).json(allTasks);
+  // console.log(allTasks);
+});
+
 router.post("/api/Task", ({ body }, res) => {
   Task.create(body)
     .then((taskdb) => {
